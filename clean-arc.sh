2aa7fd7e9f445f5640829360611b457ad0f01524
Sunil Joshi <sunilsj@gmail.com>
2023-03-13 06:20:02 -0400

    Documents

---
+++ clean-arc.sh
@@ -0,0 +1,4 @@
+rm -rf /Volumes/SUNIL/Home/Library/Application\ Support/Arc/ArchiveItemsFaviconCache
+rm -rf /Volumes/SUNIL/Home/Library/Application\ Support/Arc/ArchiveSnapshotCache
+rm -rf /Volumes/SUNIL/Home/Library/Application\ Support/Arc/StorableArchive*.json
+
